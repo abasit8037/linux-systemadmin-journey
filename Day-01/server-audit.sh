bf685861f44230e618a1c46bc28f15561e254489
abasit8037 <abdulbasit8037@gmail.com>
2026-05-12 14:00:14 +0500

    Update .sh file

--- Day-01/server-audit.sh
+++ Day-01/server-audit.sh
@@ -18,10 +18,10 @@ uname -a | tee -a $REPORT
 uptime | tee -a $REPORT
 
 echo -e "\n[2] DISK USAGE (Space)" | tee -a $REPORT
-df -hT | tee -a $REPORT
+df -hT | grep -v 'tmpfs' | grep -Ev '/dev/sdb1|efivarfs' | tee -a $REPORT
 
 echo -e "\n[3] DISK USAGE (Inodes)" | tee -a $REPORT
-df -i | tee -a $REPORT
+df -i | grep -v 'tmpfs' | grep -Ev '/dev/sdb1|efivarfs'| tee -a $REPORT
 
 echo -e "\n[4] TOP SPACE CONSUMERS in /var" | tee -a $REPORT
 du -sh /var/* 2>/dev/null | sort -rh | head -5 | tee -a $REPORT
@@ -29,20 +29,23 @@ du -sh /var/* 2>/dev/null | sort -rh | head -5 | tee -a $REPORT
 echo -e "\n[5] OPEN LISTENING PORTS" | tee -a $REPORT
 ss -tlnp | tee -a $REPORT
 
-echo -e "\n[6] LAST 10 FAILED LOGIN ATTEMPTS" | tee -a $REPORT
-grep "Failed password" /var/log/auth.log 2>/dev/null | tail -10 | tee -a $REPORT
+echo -e "\n[6] LAST 5 FAILED LOGIN ATTEMPTS" | tee -a $REPORT
+grep "Failed password" /var/log/auth.log 2>/dev/null | tail -5 | tee -a $REPORT
 
 echo -e "\n[7] /etc FILES CHANGED IN LAST 24 HOURS" | tee -a $REPORT
 find /etc -mtime -1 -type f 2>/dev/null | tee -a $REPORT
 
-echo -e "\n[8] LAST 10 LOGINS" | tee -a $REPORT
-last -n 10 | tee -a $REPORT
+echo -e "\n[8] LAST 5 LOGINS" | tee -a $REPORT
+last -n 5 | tee -a $REPORT
 
 echo -e "\n[9] CURRENTLY LOGGED IN USERS" | tee -a $REPORT
 w | tee -a $REPORT
 
 echo -e "\n[10] TOP 5 CPU PROCESSES" | tee -a $REPORT
-ps aux --sort=-%cpu | head -6 | tee -a $REPORT
+ps -eo pid,user,%cpu,%mem,comm --sort=-%cpu | column -t | head
+
+echo -e "\n[10] TOP 5 MEM PROCESSES" | tee -a $REPORT
+ps -eo pid,user,%cpu,%mem,comm:30 --sort=-%mem | head
 
 echo -e "\n========================================" | tee -a $REPORT
 echo "  Report saved: $REPORT"
